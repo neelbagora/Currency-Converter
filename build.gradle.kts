697b09ba4d661b1dd95a26f85c6b75752b2c5e21
LookLotsOfPeople <LookLotsOfPeople@users.noreply.github.com>
2019-04-30 15:55:48 -0700

    Initial Commit

---
+++ build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    kotlin("jvm") version "1.3.31"
+}
+
+group = "com.beyondbell"
+version = "1.0.0"
+
+repositories {
+    jcenter()
+}
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+}
---
+++ src/main/java/com/beyondbell/currencyconverter/Main.kt
@@ -0,0 +1,5 @@
+package com.beyondbell.currencyconverter
+
+fun main() {
+	
+}
