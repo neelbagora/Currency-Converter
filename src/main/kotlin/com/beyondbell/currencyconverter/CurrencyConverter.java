c8e98dc3c67ae7d02cae045cf0c94abd12228344
neelbagora <neelbagora@gmail.com>
2019-05-04 21:37:49 -0700

    UI

---
+++ src/main/kotlin/com/beyondbell/currencyconverter/CurrencyConverter.java
@@ -0,0 +1,16 @@
+package com.beyondbell.currencyconverter;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class CurrencyConverter extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
