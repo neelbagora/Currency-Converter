e9c382037947dc92c933cc6ba08e5a2063f87e83
neelbagora <neelbagora@gmail.com>
2019-05-06 16:55:37 -0700

    sdaflkjsadf

--- src/main/kotlin/com/beyondbell/currencyconverter/CurrencyConverter.kt
+++ src/main/kotlin/com/beyondbell/currencyconverter/CurrencyConverter.kt
@@ -3,7 +3,11 @@ package com.beyondbell.currencyconverter
 import javafx.application.Application
 import javafx.fxml.FXMLLoader
 import javafx.scene.Scene
+import javafx.scene.control.TextArea
 import javafx.stage.Stage
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 fun main() = Application.launch(CurrencyConverter::class.java)
 
@@ -11,5 +15,14 @@ internal class CurrencyConverter : Application() {
 	override fun start(primaryStage: Stage) {
 		primaryStage.scene = Scene(FXMLLoader(javaClass.getResource("/currency_converter.fxml")).load())
 		primaryStage.show()
+	}
+
+	lateinit var textBox1: TextArea
+
+	init {
+		GlobalScope.launch {
+			delay(500)
+			println(this@CurrencyConverter::textBox1.isInitialized)
+		}
 	}
 }
