60633c0484cb0ac0d14525f01c8216686b1a7f6e
LookLotsOfPeople <LookLotsOfPeople@users.noreply.github.com>
2019-05-06 20:50:01 -0700

    Removed Key

--- src/main/kotlin/com/beyondbell/currencyconverter/api/rates/RequestRates.kt
+++ src/main/kotlin/com/beyondbell/currencyconverter/api/rates/RequestRates.kt
@@ -1,8 +1,8 @@
 package com.beyondbell.currencyconverter.api.rates
 
 import com.beyondbell.currencyconverter.api.API
-import com.beyondbell.currencyconverter.api.KEY
 import com.beyondbell.currencyconverter.api.client
+import com.beyondbell.currencyconverter.key
 import io.ktor.client.request.get
 import io.ktor.client.request.parameter
 
@@ -10,5 +10,5 @@ import io.ktor.client.request.parameter
  * @param date YYYY-MM-DD
  */
 internal suspend fun requestRates(date: String = "latest") = client.get<RatesResponse>("$API/$date") {
-	parameter("access_key", KEY)
+	parameter("access_key", key)
 }.rates ?: error("Failed API Request for $date!")
